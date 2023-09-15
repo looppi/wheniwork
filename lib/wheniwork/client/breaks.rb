eb54bf14dcc54c89db6f6699bcfa04b912fe4a76
Teppo Naakka <teppo.naakka@gmail.com>
2023-09-15 16:06:09 +0300

    Fix handling with new ruby

--- lib/wheniwork/client/breaks.rb
+++ lib/wheniwork/client/breaks.rb
@@ -3,8 +3,8 @@ module WhenIWork
   class Client
     module Breaks
       def breaks_by_criteria(start_date, end_date, page = 1, params = {}, options = {})
-        start_ts = URI.encode(start_date.beginning_of_day.iso8601)
-        end_ts = URI.encode(end_date.end_of_day.iso8601)
+        start_ts = CGI.escape(start_date.beginning_of_day.iso8601)
+        end_ts = CGI.escape(end_date.end_of_day.iso8601)
         all_params = {
             'startTime': start_ts,
             'endTime': end_ts,
